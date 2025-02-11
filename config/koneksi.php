53620aaa481b7129d092d12e025de62a272f4fca
mhmdkamal9 <louisputra86@gmail.com>
2025-02-11 11:58:10 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";  
 $pass = "";     
-$db   = "kamal_if10_kepegawaian";
+$db   = "db_pegawai";
 
 // Buat koneksi
 $koneksi = mysqli_connect($host, $user, $pass, $db);
